f3c384bce41de7f82472ac48b20eaf44a6ed60ec
Chris Miles <chris.miles.e@gmail.com>
2026-09-04 23:09:01 +0000

    fix: fail closed for module discovery

--- .github/scripts/check-all-modules.sh
+++ .github/scripts/check-all-modules.sh
@@ -6,8 +6,11 @@ while IFS= read -r -d '' module; do
   [[ -n "${module_dir}" ]] || module_dir="."
   (
     cd "${module_dir}"
-    mapfile -t packages < <(GOWORK=off go list ./...)
-    [[ "${#packages[@]}" -eq 0 ]] || GOWORK=off go run golang.org/x/vuln/cmd/govulncheck@v1.7.0 -test "${packages[@]}"
+    packages_output="$(GOWORK=off go list ./...)"
+    if [[ -n "${packages_output}" ]]; then
+      mapfile -t packages <<< "${packages_output}"
+      GOWORK=off go run golang.org/x/vuln/cmd/govulncheck@v1.7.0 -test "${packages[@]}"
+    fi
   ) < /dev/null
   module_count=$((module_count + 1))
 done < <(find . -type f -name go.mod -not -path './.git/*' -not -path './node_modules/*' -not -path './vendor/*' -print0)
